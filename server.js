4965e74a2697284b6f0039ba73503146605d7263
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-04-24 17:39:17 -0500

    old code

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
 const express = require('express');
+const fs = require('fs');
+const uuid = require('uuid');
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-// Parse URL encoded & JSON
-app.use(express.urlencoded({ extended: true }));
+//middleware to parse JSON requests
 app.use(express.json());
+//middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Host public folder
-app.use(express.static('public'));
+const { v4: uuidv4 } = require('uuid');
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//route to return the notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
+//route to return the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+//Define get route to read db.json file and return saved notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).end();
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+//Define the POST route to receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client:
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).end();
+        }
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile('db.json', JSON.stringify(notes), err => {
+            if (err) {
+                console.error(err);
+                return res.status(500).end();
+            }
+            res.json(newNote);
+        });
+    });
+});
+
+
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`Server listening on port ${PORT}`);
 });
